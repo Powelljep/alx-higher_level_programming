0d48d481b0ed402b399f108b3a3d977a0f38e4f5
John Powell Odhiambo <powell.john2011@gmail.com>
2024-03-13 09:21:45 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter == 13 or letter == 101:
+    if letter == 13 and letter == 101:
         continue
     else:
         print('{:c}'.format(letter), end='')
