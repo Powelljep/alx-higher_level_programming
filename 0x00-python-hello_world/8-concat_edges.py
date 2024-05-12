e35eb02fa696b217e8b3b4c4a3ec041bbbe8960e
John Powell Odhiambo <powell.john2011@gmail.com>
2024-05-12 04:45:26 -0700

    8. Create a new sentence

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -2,6 +2,6 @@
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:66],end="")
-print(str[-22:-18],end="")
+print(str[39:66],end=" ")
+print(str[-22:-18],end=" ")
 print(str[:6])
