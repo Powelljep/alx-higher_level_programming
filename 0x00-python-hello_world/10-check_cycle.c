f646c319edb603b18400e74bc45aacbb2cff8580
John Powell Odhiambo <powell.john2011@gmail.com>
2024-05-15 07:18:55 -0700

    10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+/**
+ * check_cycle -  checks if a singly linked list has a cycle in it.
+ * @list: head of the list to check
+ * Description: checks if a singly linked list has a cycle in it
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int main()
+{
+}
