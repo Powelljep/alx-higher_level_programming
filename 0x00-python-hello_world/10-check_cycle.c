7557b65111f014038835a0fb6ebe3eb4ac72a124
John Powell Odhiambo <powell.john2011@gmail.com>
2024-05-16 03:17:04 -0700

    10. Linked list cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -5,6 +5,20 @@
  * Description: checks if a singly linked list has a cycle in it
  * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-int main()
+int check_cycle(listint_t *list)
 {
+	listint_t *fast_list, *slow_list;
+
+	if (list ==  NULL && list->next == NULL)
+		return 0;
+	fast_list = list;
+	slow_list = list;
+	while (fast_list != NULL && fast_list->next != NULL)
+	{
+		slow_list = slow_list->next;
+		fast_list = fast_list->next;
+		if (slow_list == fast_list)
+			return 1;
+	}
+	return 0;
 }
